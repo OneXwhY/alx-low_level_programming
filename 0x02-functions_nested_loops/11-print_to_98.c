2d36ede84cffa762baf554b9975a18080d27c50e
OneXwhY <akinpeluolasunkanmi@gmail.com>
2022-09-13 15:55:43 -0700

    Battery street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,18 +14,18 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-				printf("%d, ", i);
+				_putchar("%d, ", i);
 			else if (i == 98)
-				printf("%d\n", i);
+				_putchar("%d\n", i);
 		}
 	} else if (n >= 98)
 	{
 		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
-				printf("%d, ", j);
+				_putchar("%d, ", j);
 			else if (j == 98)
-				printf("%d\n", j);
+				_putchar("%d\n", j);
 		}
 	}
 }
