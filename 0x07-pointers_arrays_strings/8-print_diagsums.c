ea26e22884fd206d2e064ee80844d3d6497ea63e
OneXwhY <akinpeluolasunkanmi@gmail.com>
2022-09-26 15:00:12 -0700

    Diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -5,9 +5,9 @@
  * print_diagsums - prints the sum of the two diagonals
  * of a square matrix of integers
  * @a: square matrix of which we print the sum of diagonals
- * @size: size of the matrix 
+ * @size: size of the matrix
  */
-void print_diagsums(int *a, intsize)
+void print_diagsums(int *a, int size)
 {
 	int i;
 
