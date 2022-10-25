37fff81493deb4850ddcd9a9c356e3c7a7da1d59
OneXwhY <akinpeluolasunkanmi@gmail.com>
2022-10-25 05:40:36 -0700

    no space before tab said betty

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,12 +2,12 @@
 
 /**
  * delete_nodeint_at_index - deletes the node by a given
- * 			     index of a linked list
+ * index of a linked list
  * @head : pointer to the head of the list
  * @index: index of the node to be deleted
- * 	   (indices starting at 0)
+ * (indices starting at 0)
  * Return: 1 - if function succeeds,
- * 	  -1 - if function fails
+ * -1 - if function fails
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
